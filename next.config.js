da7fbcdc2f47b801ed8027cadf180a760d6f640c
Ismael <mael.dazareia@gmail.com>
2020-09-09 17:44:56 -0300

    fix: fix asset prefix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   esModule: true,
-  assetPrefix:
-    process.env.NODE_ENV === 'production'
-      ? 'https://raw.githubusercontent.com/maeldz/maeldz.github.io/master/'
-      : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 });
